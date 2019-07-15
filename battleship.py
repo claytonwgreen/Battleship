0407f6b22f47227c00a394bb4beac5c096b83f8b
Clayton Green <clayton.green26@gmail.com>
2019-07-15 14:29:05 -0700

    add print board functionality

--- battleship.py
+++ battleship.py
@@ -5,6 +5,7 @@ MAX_COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 MAX_ROWS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
 HOST = '127.0.0.1'
 PORT = 11223
+BOARD = {}
 
 def setup_handler():
 	print("Will you be starting the game or joining someone else's game? (start/join)")
@@ -17,9 +18,6 @@ def setup_handler():
 		global ROWS
 		COLUMNS = MAX_COLUMNS[:num]
 		ROWS = MAX_ROWS[:num]
-		print(COLUMNS)
-		print(ROWS)
-
 	else:
 		print("Please input the IP address of your opponent, then hit \"Enter\" (127.0.0.1 if playing on the same computer):")
 		OPPONENT = sys.stdin.readline().strip('\n')
@@ -30,9 +28,32 @@ def setup_handler():
 		else:
 			print("playing nonlocally")
 
+def initialize_board():
+	for row in ROWS:
+		BOARD[row] = {}
+		for col in COLUMNS:
+			BOARD[row][col] = ' '
+
+def print_board():
+	print('  ', end='| ')
+	for column in COLUMNS:
+		print("{}".format(column), end=' | ')
+	print_sep()
+	for row in ROWS:
+		print('{} |'.format(row), end='')
+		for col in COLUMNS:
+			print(' {} |'.format(BOARD[row][col]), end='')
+		print_sep()
+
+def print_sep():
+	print('\n---', end='')
+	for i in COLUMNS:
+		print('----', end='')
+	print('', end='\n')
+
 def main():
 	setup_handler()
-
+	print_board()
 
 
 
