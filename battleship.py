054b8200856179c8cdf5223807e0096ed0106350
Clayton Green <clayton.green26@gmail.com>
2019-07-17 23:11:35 -0700

    register a move input

--- battleship.py
+++ battleship.py
@@ -1,19 +1,36 @@
 import sys
 import socket
 import os
+import copy
 
 MAX_COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
 MAX_ROWS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+DIRECTIONS = ['up', 'down', 'left', 'right']
 HOST = '127.0.0.1'
 PORT = 11223
 BOARD = {}
-SHIP_SIZE = {
-	'Carrier' :  5,
-	'Battleship' : 4,
-	'Destroyer' : 3,
-	'Patrol Boat': 2
+SHIPS = {
+	'Carrier' :  {
+		'size': 5,
+		'pos': None,
+		'dir': None
+	},
+	'Battleship' : {
+		'size': 4,
+		'pos': None,
+		'dir': None
+	},
+	'Destroyer' : {
+		'size': 3,
+		'pos': None,
+		'dir': None
+	},
+	'Patrol Boat': {
+		'size': 2,
+		'pos': None,
+		'dir': None
+	}
 }
-SHIPS = {}
 
 # setup up game, whether joining game or creating new one
 def setup_handler():
@@ -27,10 +44,18 @@ def setup_handler():
 		response = sys.stdin.readline().strip('\n')
 
 	os.system('clear')
+
 	# if starting a new game
 	if response == 'start':
 		print("How many rows/columns would you like on the board? (10 is recommended, 20 is max, 7 is min)")
-		num = int(sys.stdin.readline().strip('\n'))
+		num = None
+		while True:
+			try:
+				num = int(sys.stdin.readline().strip('\n'))
+				break
+			except ValueError:
+				print("Sorry, you must input a number like \"10\"")
+				pass
 
 		# wait until user enters valid number
 		while num > 20 or num < 7:
@@ -64,23 +89,100 @@ def initialize_board():
 	for row in ROWS:
 		BOARD[row] = {}
 		for col in COLUMNS:
-			BOARD[row][col] = ' '
+			BOARD[row][col] = '   '
 
 
 # allow user to place their ships on the board
 def place_ships():
+	available_ships = list(SHIPS.keys())
+
 	print_board()
-	print("Choose where to place your first ship by typing in the boat name, location, and direction. \nAn example is \"Destroyer 2A down\"\n")
-	print("Ships:\n\tCarrier: <===> \n\tBattleship: <==> \n\tDestroyer: <=> \n\tPatrol Boat: <>\n")
+	print("Choose where to place your available ships, one at a time, by typing in the boat name, location, and direction.")
+	print("An example is \"Destroyer 2A down\"\n")
+
+	print("Available ships:")
+	for ship in available_ships:
+		print("\t{} (size {})".format(ship, SHIPS[ship]['size']))
+	print('', end='\n')
+
 	location = sys.stdin.readline().strip('\n').split(' ')
+
+	if len(location) != 3:
+		print("Must pass in three values")
+	elif location[0] not in available_ships:
+		print("Sorry, you must input a valid ship name")
+	elif not valid_location(location[1]):
+		print("invalid location")
+	elif location[2] not in DIRECTIONS:
+		print("invalid direction")
+	elif not ship_fits_on_board(location[0], location[1], location[2]):
+		print("{} does not fit at that placement".format(location[0]))
+
 	print(location)
+
+
+def valid_location(location_string):
+	index = len(location_string) - 1
+
+	if location_string[index] not in COLUMNS:
+		return False
+
+	if location_string[:index] not in ROWS:
+		return False
+
+	return True
+
+
+# ensure valid placement of ship
+def ship_fits_on_board(ship_type, location_string, direction):
+	index = len(location_string) -1
+
+	if direction == 'down' or direction == 'up':
+		start_row = int(location_string[:index])
+		diff = SHIPS[ship_type]['size'] - 1
+		end_row = None
+		if direction == 'down':
+			end_row = start_row + diff
+		else:
+			end_row = start_row - diff
+		if end_row > MAX_LENGTH or end_row < 1:
+			print("MAX_LENGTH is {}".format(MAX_LENGTH))
+			print("start row is {}".format(start_row))
+			print("diff is {}".format(diff))
+			print("end row is {}".format(end_row))
+			print("invalid row")
+			return False
+
+	else:
+		start_col = COLUMNS.index(location_string[index])
+		diff = SHIPS[ship_type]['size'] - 1
+		end_col = None
+		if direction == 'right':
+			end_col = start_col + diff
+		else:
+			end_col = start_col - diff
+		if end_col >= MAX_LENGTH or end_col < 0:
+			print("MAX_LENGTH is {}".format(MAX_LENGTH))
+			print("start col is {}".format(start_col))
+			print("diff is {}".format(diff))
+			print("end col is {}".format(end_col))
+			print("invalid row")
+
+
+	return True
+
+
+
+
+
+
 
 
 # print out the current state of the board
 def print_board():
-	print('   ', end='| ')
+	print('   ', end='|')
 	for column in COLUMNS:
-		print("{}".format(column), end=' | ')
+		print(" {} ".format(column), end='|')
 	print_sep()
 	for row in ROWS:
 		if len(row) == 1:
@@ -88,7 +190,7 @@ def print_board():
 		else:
 			print('{} |'.format(row), end='')
 		for col in COLUMNS:
-			print(' {} |'.format(BOARD[row][col]), end='')
+			print('{}|'.format(BOARD[row][col]), end='')
 		print_sep()
 
 # helper for print_board()
