c9b6c6eadcc38b6c55948e1c1e81a88a7b334753
Clayton Green <clayton.green26@gmail.com>
2019-07-18 17:39:16 -0700

    add socket connection

--- battleship.py
+++ battleship.py
@@ -1,7 +1,8 @@
 import sys
 import socket
+import pickle
 import os
-import copy
+import time
 
 MAX_COLUMNS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T']
 MAX_ROWS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
@@ -23,6 +24,10 @@ SHIPS = {
 		'size': 2
 	}
 }
+OPPONENT = None
+OPPONENT_PORT = None
+PLAYER_NAME = None
+OPPONENT_NAME = None
 
 #################################################################################
 ################################ GAME SETUP #####################################
@@ -30,17 +35,23 @@ SHIPS = {
 
 # setup up game, whether joining game or creating new one
 def setup_handler():
+	os.system('clear')
+
 	# see if player is joining a game or starting a new one
 	print("Will you be starting the game or joining someone else's game? (start/join)")
 	response = sys.stdin.readline().strip('\n')
 
 	# wait until user enters a valid response
 	while response != 'start' and response != 'join':
-
-
-
 		print("Sorry, you must specify either 'start' or 'join' to continue")
 		response = sys.stdin.readline().strip('\n')
+
+	os.system('clear')
+
+	# have player enter their name
+	print("Please enter your name:")
+	global PLAYER_NAME
+	PLAYER_NAME = sys.stdin.readline().strip('\n')
 
 	os.system('clear')
 
@@ -72,13 +83,22 @@ def setup_handler():
 	# if joining a game
 	else:
 		print("Please input the IP address of your opponent, then hit \"Enter\" (127.0.0.1 if playing on the same computer):")
+		global OPPONENT
 		OPPONENT = sys.stdin.readline().strip('\n')
-		print("opponent at {}".format(OPPONENT))
 
 		if OPPONENT == '127.0.0.1':
 			print("playing locally")
 		else:
 			print("playing nonlocally")
+
+		print("Please input your opponents port number. If then did not enter one, enter \"Default\"")
+		global OPPONENT_PORT
+		OPPONENT_PORT = sys.stdin.readline().strip('\n')
+		if OPPONENT_PORT == 'Default':
+			OPPONENT_PORT = 11223
+
+		print("opponent at {} port {}".format(OPPONENT, OPPONENT_PORT))
+
 
 	os.system('clear')
 	return
@@ -101,36 +121,70 @@ def initialize_board():
 # allow user to place their ships on the board
 def place_ships():
 	available_ships = list(SHIPS.keys())
+	os.system('clear')
+	message = ''
 
-	print_board()
-	print("Choose where to place your available ships, one at a time, by typing in the boat name, location, and direction.")
-	print("An example is \"Destroyer 2A down\"\n")
-
+	# allow player to place ships until they have run out
 	while len(available_ships) != 0:
-		print("Available ships:")
-		for ship in available_ships:
-			print("\t{} (size {})".format(ship, SHIPS[ship]['size']))
-		print('', end='\n')
+		print_current_state(available_ships)
 
-		location = sys.stdin.readline().strip('\n').split(' ')
+		if message != '':
+			print("ERROR: {}".format(message), end='\n\n')
+		
+		print("Input your placement: ", end='')
+		sys.stdout.flush()
+		message = ''
+		location_array = sys.stdin.readline().strip('\n').split(' ')
 
-		if len(location) != 3:
-			print("Must pass in three values")
-		elif location[0] not in available_ships:
-			print("Sorry, you must input a valid ship name")
-		elif not valid_location(location[1]):
-			print("invalid location")
-		elif location[2] not in DIRECTIONS:
-			print("invalid direction")
-		elif not ship_fits_on_board(location[0], location[1], location[2]):
-			print("{} does not fit at that placement".format(location[0]))
+		if len(location_array) == 1 and location_array[0] == 'clear':
+			initialize_board()
+			available_ships = list(SHIPS.keys())
+			os.system('clear')
+		elif len(location_array) != 3:
+			os.system('clear')
+			message = "Sorry, you must either pass in three values or \"clear\""
+		elif location_array[0] not in available_ships:
+			os.system('clear')
+			message = "Sorry, {} is not a valid ship".format(location_array[0])
+		elif not valid_location(location_array[1]):
+			os.system('clear')
+			message = "Sorry, {} is not a valid location".format(location_array[1])
+		elif location_array[2] not in DIRECTIONS:
+			os.system('clear')
+			message = "Sorry, {} is not a valid direction".format(location_array[2])
+		elif not ship_fits_on_board(location_array[0], location_array[1], location_array[2]):
+			os.system('clear')
+			message = "{} does not fit at {} with direction {}".format(location_array[0], location_array[1], location_array[2])
 		else: 
-			place_ship(location[0], location[1], location[2])
-			available_ships.remove(location[0])
-			print_board()
-			print("Please make your next selection")
+			place_ship(location_array[0], location_array[1], location_array[2])
+			available_ships.remove(location_array[0])
+			os.system('clear')
 
-	print(location)
+# print for ship placement
+def print_current_state(available_ships):
+	# print curretn board
+	print_board()
+
+	# print instructions
+	print('', end='\n')
+	print("Choose a ship placement using the format SHIP ROW+COL DIRECTION")
+	print("Example: \"Destroyer 2A down\"")
+	print('', end='\n')
+
+	# print list of ships player can still use
+	print("Available ships:")
+	for ship in available_ships:
+		print("\t{} (size {})".format(ship, SHIPS[ship]['size']))
+	print('', end='\n')
+
+	# print directions ship can point
+	print("Possible directions:")
+	for direction in DIRECTIONS:
+		print("\t{}".format(direction))
+	print('', end='\n')
+
+	# print clear option to restart
+	print("Enter \"clear\" to clear your current placements", end='\n\n')
 
 # ensure location string is valid position on board
 def valid_location(location_string):
@@ -149,27 +203,28 @@ def ship_fits_on_board(ship_type, location_string, direction):
 	index = len(location_string) - 1
 	start_row = int(location_string[:index])
 	start_col = COLUMNS.index(location_string[index])
+	diff = SHIPS[ship_type]['size'] - 1
 
 	if direction == 'down' or direction == 'up':
-		diff = SHIPS[ship_type]['size'] - 1
 		end_row = None
 		if direction == 'down':
 			end_row = start_row + diff
 		else:
 			end_row = start_row - diff
 		if end_row > MAX_LENGTH or end_row < 1:
+			print("return 1")
 			return False
 
-		for i in range(SHIPS[ship_type]['size']):
-			if direction == 'down' and BOARD[ROWS[start_row + i]][COLUMNS[start_col]] == ' x ':
-				return False
-			elif BOARD[ROWS[start_row - i]][COLUMNS[start_col]] == ' x ':
-				return False
+		for i in range(diff + 1):
+			if direction == 'down':
+				if BOARD[ROWS[start_row - 1 + i]][COLUMNS[start_col]] != '   ':
+					return False
+			else:
+				if BOARD[ROWS[start_row - 1 - i]][COLUMNS[start_col]] != '   ':
+					return False
 
-
-
-	else:
-		diff = SHIPS[ship_type]['size'] - 1
+	# direction == 'left' or 'right'
+	else: 
 		end_col = None
 		if direction == 'right':
 			end_col = start_col + diff
@@ -178,11 +233,13 @@ def ship_fits_on_board(ship_type, location_string, direction):
 		if end_col >= MAX_LENGTH or end_col < 0:
 			return False
 
-		for i in range(SHIPS[ship_type]['size']):
-			if direction == 'right' and BOARD[ROWS[start_row]][COLUMNS[start_col + i]] == ' x ':
-				return False
-			elif BOARD[ROWS[start_row]][COLUMNS[start_col - i]] == ' x ':
-				return False
+		for i in range(diff + 1):
+			if direction == 'right':
+				if BOARD[ROWS[start_row - 1]][COLUMNS[start_col + i]] != '   ':
+					return False
+			else:
+				if BOARD[ROWS[start_row - 1]][COLUMNS[start_col - i]] != '   ':
+					return False
 
 
 	return True
@@ -222,7 +279,7 @@ def place_ship(ship_type, location_string, direction):
 			elif i == size - 1:
 				BOARD[ROWS[row]][COLUMNS[col - i]] = ' <='
 			else:
-				BOARD[ROWS[row]][COLUMNS[col] - i] = '==='
+				BOARD[ROWS[row]][COLUMNS[col - i]] = '==='
 
 #################################################################################
 ############################## PRINTING BOARD ###################################
@@ -250,7 +307,7 @@ def print_board():
 def print_sep():
 	print('\n----', end='')
 	for i in COLUMNS:
-		print('----', end='')
+		print(' - -', end='')
 	print('', end='\n')
 
 
@@ -259,11 +316,45 @@ def print_sep():
 #################################################################################
 
 def main():
-	os.system('clear')
+	# setup game
 	setup_handler()
+
+	# set up socket
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	global OPPONENT_NAME
+
+	# connect or wait for opponent to connect
+	if OPPONENT == None:
+		s.bind((HOST, PORT))
+		print("binding to {} at port {}".format(HOST, PORT))
+		s.listen()
+		conn, addr = s.accept()
+		print('Connected by', addr)
+		OPPONENT_NAME = conn.recv(1024).decode()
+		print("playing against {}".format(OPPONENT_NAME))
+		hello_msg = PLAYER_NAME.encode()
+		conn.sendall(hello_msg)
+		conn.sendall("12".encode())
+		time.sleep(10)
+
+	else:
+		print("Trying to connect to {} at port {}".format(OPPONENT, OPPONENT_PORT))
+		sys.stdout.flush()
+		s.connect((OPPONENT, OPPONENT_PORT))
+		hello_msg = PLAYER_NAME.encode()
+		s.sendall(hello_msg)
+		OPPONENT_NAME = s.recv(1024).decode()
+		print("playing against {}".format(OPPONENT_NAME))
+		global MAX_LENGTH
+		MAX_LENGTH = int(s.recv(1024).decode())
+		print("max length is {}".format(MAX_LENGTH))
+		time.sleep(10)
+
+	# allow player to place thier ships
 	initialize_board()
 	place_ships()
-	print_board()
+
+	
 
 
 
@@ -271,17 +362,18 @@ def main():
 
 
 
-	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	    s.bind((HOST, PORT))
-	    s.listen()
-	    conn, addr = s.accept()
-	    with conn:
-	        print('Connected by', addr)
-	        while True:
-	            data = conn.recv(1024)
-	            if not data:
-	                break
-	            conn.sendall(data)
+
+	# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	#     s.bind((HOST, PORT))
+	#     s.listen()
+	#     conn, addr = s.accept()
+	#     with conn:
+	#         print('Connected by', addr)
+	#         while True:
+	#             data = conn.recv(1024)
+	#             if not data:
+	#                 break
+	#             conn.sendall(data)
 
 if __name__ == '__main__':
 	main()
